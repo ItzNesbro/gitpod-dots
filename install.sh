4c92a99dcbbc4bd9752813e67a61ff200933cb20
Nesbeer <150609205+ItzNesbro@users.noreply.github.com>
2024-12-16 15:42:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
 
-# Install neovim
+set -e  # Exit on error
+set -o pipefail  # Catch errors in piped commands
+
+echo "Starting automated setup..."
+
+# Update and install prerequisites
+echo "Updating package list..."
+sudo apt update -y && sudo apt upgrade -y
+
+# Install Neovim
+echo "Installing Neovim..."
 sudo apt remove vim -y
-brew install neovim -y
-sudo ln -s $(which nvim) /usr/bin/vim
+if ! command -v brew &>/dev/null; then
+  echo "Homebrew not found. Installing Homebrew..."
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
+fi
+brew install neovim
+sudo ln -sf "$(which nvim)" /usr/bin/vim
 
 # Install tmux
+echo "Installing tmux..."
 sudo apt install tmux -y
 
 # Install eza
-sudo apt install eza  -y
+echo "Installing eza..."
+brew install eza
 
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing Oh My Zsh..."
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" </dev/null
+else
+  echo "Oh My Zsh already installed."
+fi
 
 # Clone and setup dotfiles
-cd ~/
-git clone https://github.com/ItzNesbro/dotfiles ~/dotfiles
-mv ~/dotfiles/.config/nvim ~/.config/nvim
+echo "Cloning dotfiles repository..."
+DOTFILES_DIR="$HOME/dotfiles"
+if [ ! -d "$DOTFILES_DIR" ]; then
+  git clone https://github.com/ItzNesbro/dotfiles "$DOTFILES_DIR"
+else
+  echo "Dotfiles repository already cloned."
+fi
+
+echo "Setting up configuration files..."
+mkdir -p ~/.config
+cp -r "$DOTFILES_DIR/.config/nvim" ~/.config/nvim
 rm -rf ~/.config/tmux
-mv ~/dotfiles/.config/tmux ~/.config/tmux
+cp -r "$DOTFILES_DIR/.config/tmux" ~/.config/tmux
 
 # Update zshrc with aliases and plugins
-echo '
+echo "Configuring .zshrc..."
+cat <<'EOF' >> ~/.zshrc
+
 # Define useful aliases
 alias cl="clear"
 alias ll="eza -l -g --icons"
@@ -38,17 +69,28 @@ plugins=(
 )
 
 source $ZSH/oh-my-zsh.sh
-' >> ~/.zshrc
+EOF
 
 # Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+echo "Installing zsh-autosuggestions..."
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+else
+  echo "zsh-autosuggestions already installed."
+fi
 
 # Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+echo "Installing zsh-syntax-highlighting..."
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+else
+  echo "zsh-syntax-highlighting already installed."
+fi
 
-# Source the updated zshrc
+# Source .zshrc
+echo "Sourcing .zshrc..."
 source ~/.zshrc
 
-# install czg
-npm i -g czg
-npm i -g minimal-git-cz
+# Install czg and minimal-git-cz
+echo "Installing czg and minimal-git-cz..."
+npm install -g minimal-git-cz
